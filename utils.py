4328e2d45768a60f3d32daca591699208de072b4
endaaman <buhibuhidog@gmail.com>
2019-02-11 17:23:16 +0900

    update

--- utils.py
+++ utils.py
@@ -2,3 +2,9 @@ import datetime
 
 def now_str():
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def dice_coef(input, target, smooth = 1.):
+    iflat = input.view(-1)
+    tflat = target.view(-1)
+    intersection = (iflat * tflat).sum()
+    return (2. * intersection + smooth) / (iflat.sum() + tflat.sum() + smooth)
