b9e192176594be845df61a364b79b5727929d466
endaaman <buhibuhidog@gmail.com>
2019-02-18 13:06:13 +0900

    use bn

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import datasets, models
 from torchvision.transforms import ToTensor, Normalize, Compose
-from net import UNet11, UNet16
+from net import UNet11, UNet11bn, UNet16, UNet16bn
 from data import LaidDataset, RandomPatchDataset
 from utils import now_str, dice_coef
 
@@ -17,9 +17,16 @@ BATCH_SIZE = 32
 NUM_WORKERS = 4
 EPOCH_COUNT = 500
 MULTI_GPU = True
-NET = 'unet16'
+NET_NAME = 'unet11bn'
+NETs = {
+    'unet11': UNet11,
+    'unet16': UNet16,
+    'unet11bn': UNet11bn,
+    'unet16bn': UNet16bn,
+}
+NET = NETs[NET_NAME]
 
-print(f'Preparing NET: {NET} BATCH: {BATCH_SIZE} EPOCH: {EPOCH_COUNT} MULTI_GPU: {MULTI_GPU} ({now_str()})')
+print(f'Preparing NET: {NET_NAME} BATCH: {BATCH_SIZE} EPOCH: {EPOCH_COUNT} MULTI_GPU: {MULTI_GPU} ({now_str()})')
 
 first_epoch = 1
 weight_file = None
@@ -63,10 +70,7 @@ data_loader = DataLoader(data_set, batch_size=BATCH_SIZE, shuffle=True, num_work
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 model = None
-if NET == 'unet11':
-    model = UNet11(num_classes=NUM_CLASSES)
-else:
-    model = UNet16(num_classes=NUM_CLASSES)
+model = NET(num_classes=NUM_CLASSES)
 model = model.to(device)
 if weight_file:
     model.load_state_dict(torch.load(weight_file))
@@ -78,7 +82,7 @@ criterion = nn.BCELoss()
 
 print(f'Starting ({now_str()})')
 epoch = first_epoch
-weight_dir = f'./weights/{NET}/'
+weight_dir = f'./weights/{NET_NAME}/'
 os.makedirs(weight_dir, exist_ok=True)
 while epoch <= EPOCH_COUNT:
     message = None
