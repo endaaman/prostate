aee041fdfc7dc0060c17e9969262bf9be7ad6025
endaaman <buhibuhidog@gmail.com>
2019-06-07 19:32:04 +0900

    add web module

---
+++ web/__main__.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, render_template
+
+from net import UNet11, UNet16
+from utils import now_str, overlay_transparent, to_heatmap
+
+
+TEMPLATE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=TEMPLATE_DIR)
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='flask test', message=now_str())
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=4000)
