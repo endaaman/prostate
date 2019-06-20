b1a7e574510eb4f26610abc3584f42bda12d4106
endaaman <buhibuhidog@gmail.com>
2019-06-21 05:39:21 +0900

    update

--- train_all.sh
+++ train_all.sh
@@ -18,11 +18,11 @@ python train.py -m albunet_n -b  4 -e $epoch --tile $tile --dest $dest
 tile=256
 dest="weights/$tile"
 python train.py -m unet11    -b 20 -e $epoch --tile $tile --dest $dest
-python train.py -m unet11b   -b 16 -e $epoch --tile $tile --dest $dest
-python train.py -m unet11n   -b 16 -e $epoch --tile $tile --dest $dest
+python train.py -m unet11b   -b 12 -e $epoch --tile $tile --dest $dest
+python train.py -m unet11n   -b 12 -e $epoch --tile $tile --dest $dest
 python train.py -m unet16    -b 20 -e $epoch --tile $tile --dest $dest
-python train.py -m unet16b   -b 16 -e $epoch --tile $tile --dest $dest
-python train.py -m unet16n   -b 16 -e $epoch --tile $tile --dest $dest
+python train.py -m unet16b   -b 12 -e $epoch --tile $tile --dest $dest
+python train.py -m unet16n   -b 12 -e $epoch --tile $tile --dest $dest
 python train.py -m albunet   -b 32 -e $epoch --tile $tile --dest $dest
 python train.py -m albunet_b -b 16 -e $epoch --tile $tile --dest $dest
 python train.py -m albunet_n -b 16 -e $epoch --tile $tile --dest $dest
