9a38e9e43eeccedde19f132fb5bdbccd5058b369
endaaman <buhibuhidog@gmail.com>
2019-06-24 14:18:39 +0900

    update

--- infer.py
+++ infer.py
@@ -14,29 +14,34 @@ from store import Store
 from utils import now_str, curry, to_heatmap, overlay_transparent
 
 
+NUM_CLASSES = 5
+
 parser = argparse.ArgumentParser()
 parser.add_argument('input')
 parser.add_argument('-w', '--weight')
 parser.add_argument('-m', '--model')
-parser.add_argument('--single-gpu', action="store_true")
 parser.add_argument('--cpu', action="store_true")
+parser.add_argument('-d', '--dest', default='out')
 args = parser.parse_args()
 
 INPUT_PATH = args.input
 WEIGHT_PATH = args.weight
-USE_GPU = not args.cpu and torch.cuda.is_available()
-USE_MULTI_GPU = USE_GPU and not args.single_gpu
 MODEL_NAME = args.model
-NUM_CLASSES = 5
-mode = ('multi' if USE_MULTI_GPU else 'single') if USE_GPU else 'cpu'
-print(f'Preparing MODEL:{MODEL_NAME} GPU:{USE_GPU} MODE: {mode} NUM_CLASSES:{NUM_CLASSES} ({now_str()})')
+DEST_BASE_DIR = args.dest
 
+USE_GPU = not args.cpu and torch.cuda.is_available()
+USE_MULTI_GPU = USE_GPU and torch.cuda.device_count() > 1
+
+DEST_DIR = os.path.join(DEST_BASE_DIR, MODEL_NAME)
+
+mode = ('multi' if USE_MULTI_GPU else 'single') if USE_GPU else 'cpu'
+print(f'Preparing MODEL:{MODEL_NAME} MODE:{mode} NUM_CLASSES:{NUM_CLASSES} TARGET:{INPUT_PATH} ({now_str()})')
 
 
 def add_padding(img):
     h, w = img.shape[0:2]
-    new_w = math.ceil(w / 32) * 32
-    new_h = math.ceil(h / 32) * 32
+    new_w = math.ceil(w / 64) * 64
+    new_h = math.ceil(h / 64) * 64
     left = (new_w - w) // 2
     right = (new_w - w) - left
     top = (new_h - h) // 2
@@ -90,14 +95,14 @@ mask_arr = post_process(output_tensor.data[0].cpu(), original_dims)
 print(f'Finished inference.')
 
 base_name = os.path.splitext(os.path.basename(INPUT_PATH))[0]
-output_dir = f'./out/{MODEL_NAME}/{base_name}'
+output_dir = f'./{DEST_BASE_DIR}/{MODEL_NAME}/{base_name}'
 os.makedirs(output_dir, exist_ok=True)
 np.save(f'{output_dir}/out.npy', mask_arr)
 mask_img = arr_to_img(mask_arr)
 cv2.imwrite(f'{output_dir}/org.jpg', input_img)
 cv2.imwrite(f'{output_dir}/out.png', mask_img)
 masked_img = overlay_transparent(input_img, mask_img)
-cv2.imwrite(f'{output_dir}/masked.png', masked_img)
+cv2.imwrite(f'{output_dir}/masked.jpg', masked_img)
 for i in range(NUM_CLASSES):
     img = to_heatmap(mask_arr[:, :, i])
     cv2.imwrite(f'{output_dir}/heat_{i}.png', img)
--- scripts/train_512.sh
+++ scripts/train_512.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # export CUDA_VISIBLE_DEVICES="0"
 
 epoch=30
