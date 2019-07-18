ca2980f8acf34ba14f7cf1945fab5b99444e108d
endaaman <buhibuhidog@gmail.com>
2019-07-18 18:06:13 +0900

    update

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ class CrossEntropyLoss2d(nn.Module):
         self.loss_fn = nn.NLLLoss()
 
     def forward(self, x, y):
-        x = x.permute(0, 2, 3, 1).contiguous().view(-1, NUM_CLASSES).log()
+        x = (x.permute(0, 2, 3, 1).contiguous().view(-1, NUM_CLASSES) + 1e-24).log()
         _, y = torch.max(y.permute(0, 2, 3, 1).contiguous().view(-1, NUM_CLASSES), -1)
         return self.loss_fn(x, y)
 
